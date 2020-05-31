7fdfb7bb7932f7e86f147ae8127db80e63cb3fac
JeannieFallon <jeanniefallon@gmail.com>
2020-05-31 16:50:15 -0400

    add result view & controller

---
+++ Controllers/ResultController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CiphersWeb.Models;
+
+namespace CiphersWeb.Controllers
+{
+    public class ResultController : Controller 
+    {
+        private readonly ILogger<ResultController> _logger;
+
+        public ResultController(ILogger<ResultController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
